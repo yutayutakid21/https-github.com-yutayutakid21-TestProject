cf0456899a22ac74e60b160cb23d843e9a50e8c5
Yuta Fujii <yutayutakid21@gmail.com>
2019-09-30 18:45:20 +0900

    ラベル追加、コード追加

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        label.text = "あいうえお！"
+        
         // Do any additional setup after loading the view.
     }
 
